15085907bb4de5e59fe9f80eb06eec7ff1ace0e4
rmahawewa <rmahawewa@yahoo.com>
2025-05-05 12:32:22 +0530

    fetch and display items all in one page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const url = 'https://dummyjson.com/products';
+
+  const fetchData = async () => {
+    let values = await fetch(url);
+    let products = await values.json();
+    console.log(products.products);
+    setData(products.products);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='items-container'>
+        {
+          data.map(d => (
+            <Card key={d.id} id={d.id} title={d.title} thumbnail={d.thumbnail} />
+          ))
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
+}
+
+function Card({id, title, thumbnail}){
+  return(
+    <div key={id} className='outer-container'>
+      <div className='inner-container'>
+        <img src={thumbnail} alt={title} />
+        <label>{title}</label>
+      </div>
+    </div>
+  );
 }
 
 export default App
